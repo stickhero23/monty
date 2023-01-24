b655d1fe8f1e1a99419c3072c8687d3363fd4b89
Sherleken <kendisherleen@gmail.com>
2023-01-24 15:22:11 +0000

    newprint.c

---
+++ _print.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+/**
+ * pall - print all nodes in stack
+ * @h: head of list
+ * @line_number: bytecode line number
+ */
+void pall(stack_t **h, unsigned int line_number)
+{
+	stack_t *tmp = NULL;
+
+	if (!h || !*h)
+		return;
+
+	(void) line_number;
+	tmp = *h;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+/**
+ * pint - print top node in stack
+ * @h: head of list
+ * @line_number: bytecode line number
+ */
+void pint(stack_t **h, unsigned int line_number)
+{
+	if (!h || !*h)
+	{
+		printf("L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*h)->n);
+}
+/**
+ * nop - do nothing
+ * @h: head of list
+ * @line_number: bytecode line number
+ */
+void nop(stack_t **h, unsigned int line_number)
+{
+	(void) h;
+	(void) line_number;
+}
